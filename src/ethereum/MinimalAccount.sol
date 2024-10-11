37edd40306e23d9d372024e9d987544a0e701345
chep <vcheplev42@gmail.com>
2024-10-11 22:31:59 +0200

    forge fmt

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -4,34 +4,28 @@ pragma solidity ^0.8.24;
 
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
-import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import { SIG_VALIDATION_FAILED } from "lib/account-abstraction/contracts/core/Helpers.sol";
-import { SIG_VALIDATION_SUCCESS } from "lib/account-abstraction/contracts/core/Helpers.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED} from "lib/account-abstraction/contracts/core/Helpers.sol";
+import {SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
 
 contract MinimalAccount is IAccount, Ownable {
-
-    constructor() Ownable(msg.sender){}
+    constructor() Ownable(msg.sender) {}
 
     // A signature is valid, if it's the MinimalAccount owner
-    function validateUserOp(
-        PackedUserOperation calldata userOp, 
-        bytes32 userOpHash, 
-        uint256 missingAccountFunds)
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
-        returns (uint256 validationData) 
-        {
-            validationData = _validateSignature(userOp, userOpHash);
-            _payPrefund(missingAccountFunds);
-        }
+        returns (uint256 validationData)
+    {
+        validationData = _validateSignature(userOp, userOpHash);
+        _payPrefund(missingAccountFunds);
+    }
 
-    function _validateSignature(
-        PackedUserOperation calldata userOp, 
-        bytes32 userOpHash) 
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
         internal
-        view 
-        returns(uint256 validationData) 
+        view
+        returns (uint256 validationData)
     {
         bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
 
@@ -42,7 +36,6 @@ contract MinimalAccount is IAccount, Ownable {
         }
 
         return SIG_VALIDATION_SUCCESS;
-
     }
 
     function _payPrefund(uint256 missingAccountFunds) internal {
